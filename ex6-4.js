6c05b2d2acad255004c795f0c4dc5e89d48e8c25
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-31 22:44:32 +0900

    Update ex6-4.js

--- ex6-4.js
+++ ex6-4.js
@@ -1,7 +1,15 @@
-function imgchange1(){
-  document.getElementById('logo').src = './images/baseball-stadium.png';
-}
+function changePhoto614() {
+  // getElementById のスペルと大文字小文字を修正
+  const myImage = document.getElementById("myPhoto1");
+  if (!myImage) return;
+    // '56.jpg' の画像パス (フォルダ名 'Image' は大文字)
+    myImage.src = "images/baseball-stadium.png";
+  }
 
-function imgReset1(){
-  document.getElementById('logo').src = './images/12552.jpg';
-}
+function returnPhoto614() {
+  // getElementById のスペルと大文字小文字を修正
+  const myImage = document.getElementById("myPhoto1");
+  if (!myImage) return;
+    // '54.jpg' の画像パス (フォルダ名 'image' は小文字)
+    myImage.src = "images/12552.jpg";
+  }
