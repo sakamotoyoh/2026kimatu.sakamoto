4022ba17b4c1eae3bbdae846087366524e89170c
sakamotoyoh <zm25357@josai.ac.jp>
2026-01-31 21:54:24 +0900

    Update ex6-3.js

--- ex6-3.js
+++ ex6-3.js
@@ -2,7 +2,7 @@ function hyoji18(){
   //表示する文字
 var str18="目標:公式戦に登板する。";
   //テキストボックスの文字数
-var cnt18=document.timer17.moji17.value.length;
+var cnt18=document.timer18.moji18.value.length;
 //文字が全部表示されているか確認
   if (cnt18<11){
     //現在より１文字多く切り出して表示
